b7ee6bf851832eacff44c7cca5c68531117b2f35
marceloost <104876213+marceloost@users.noreply.github.com>
2023-06-14 21:46:43 -0300

    up

---
+++ ifootball-front/src/app/home/page.tsx
@@ -0,0 +1,6 @@
+import styles from '../../../styles/home.module.scss';
+export default function Home(){
+    return(<div className={styles.HomePage}> 
+        alguma coisa aleatoria ali dentro
+    </div>)
+}
