defeff833a9f24478983fdd19748db5a0d1829ed
Marcelo José Griebler Ost <marcelo.ost7@gmail.com>
2023-06-21 09:09:15 -0300

    up

--- ifootball-front/src/app/homepage/page.tsx
+++ ifootball-front/src/app/homepage/page.tsx
@@ -1,9 +1,16 @@
-import styles from '../../../styles/home.module.scss';
-import Header from '@/components/Header';
+import React from "react";
+import styles from "../../../styles/home.module.scss";
+import Header from "@/components/Header";
+
 export default function Home() {
-    return (
+  return (
     <div className={styles.HomePage}>
-        <Header />
+      <Header />
+      <div>
+        <p>.</p>
+        <h3>criação de times fecha em</h3>
+        <p></p>
+      </div>
     </div>
-    )
+  );
 }
