8f04bde0bcdec7b2868d6944a22275ee9e7072c3
Otávio Maldaner <otavio.maldaner@aluno.feliz.ifrs.edu.br>
2023-06-28 09:08:48 -0300

    upload

---
+++ ifootball-front/src/components/globalCard/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../../../styles/card.module.scss';
+const GlobalCard = () => {
+    return (
+        <div className={styles.globalCard}>
+            
+        </div>
+    );
+}
+export default GlobalCard;
