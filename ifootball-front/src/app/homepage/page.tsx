3c23c1dd27ad7a879aa68671058f86659089b304
Marcelo José Griebler Ost <marcelo.ost7@gmail.com>
2023-06-28 08:33:49 -0300

    up

--- ifootball-front/src/app/homepage/page.tsx
+++ ifootball-front/src/app/homepage/page.tsx
@@ -8,8 +8,31 @@ export default function Home() {
       <Header />
       <div>
         <p>.</p>
-        <h3>criação de times fecha em</h3>
-        <p></p>
+        <div className={styles.relogio}>
+          <h3>criação de times fecha em</h3>
+        </div>
+        <div className={styles.homePageUtil}>
+          <div className={styles.time}>
+            <h1 className={styles.titulo}>Nome do Time</h1>
+            <h3 className={styles.subtitulo}>Pontução</h3>
+
+            <div className={styles.buttons}>
+              <a href="">Escalar Time M</a>
+              <a href="">Escalar Time F</a>
+            </div>
+          </div>
+
+          <div className={styles.pontuacao}>
+            <h1 className={styles.titulo}>Pontuação</h1>
+            <h3 className={styles.subtitulo}>Top 3 Times</h3>
+
+            <div className={styles.buttons}>
+              <a href="">Ver Mais</a>
+            </div>
+          </div>
+
+
+        </div>
       </div>
     </div>
   );
