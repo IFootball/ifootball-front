c0e2c07c6024861257563d5f3466305ec29d78a4
Otávio Maldaner <otavio.maldaner@aluno.feliz.ifrs.edu.br>
2023-06-28 09:08:48 -0300

    upload

--- ifootball-front/src/app/homepage/page.tsx
+++ ifootball-front/src/app/homepage/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div className={styles.HomePage}>
-      <Header />
+      {/* <Header /> */}
       <div>
         <p>.</p>
         <div className={styles.relogio}>
