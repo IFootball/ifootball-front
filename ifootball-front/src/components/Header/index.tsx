f98bb66f7a8e7acfbd65d90753a36b78e16c0d9d
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2023-07-04 20:04:00 -0300

    header

---
+++ ifootball-front/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from '../../../styles/header.module.scss'
+
+export default function Navbar() {
+  return (
+
+    <header className={styles.header}>
+        <div className={styles.content}>
+    
+            <Link className={styles.logo} href={'homepage'}><img className={styles.logoImg} src="images/logo.png" title="ifootballLogo" /></Link>
+    
+            <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" />
+            <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
+    
+            <ul className={styles.nav}>
+                <li><Link className={styles.ops} href={'homepage'}>PÁGINA INICIAL</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-2</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-3</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-4</Link></li>
+    
+            </ul>
+        </div>
+    </header>
+    
+  )
+}
