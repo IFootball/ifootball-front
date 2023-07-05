c1022344a87b091b9fc05d74bdf16aa57ca13892
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2023-07-05 09:10:12 -0300

    header2

--- ifootball-front/src/components/Header/index.tsx
+++ ifootball-front/src/components/Header/index.tsx
@@ -1,27 +1,51 @@
+'use client'
 import Link from 'next/link'
 import styles from '../../../styles/header.module.scss'
+import Home from '@/app/page'
+import { useState } from 'react'
+import { link } from 'fs'
 
 export default function Navbar() {
+
+  const [openSideBar, setOpenSideBar] = useState<boolean>(false)
+
   return (
 
     <header className={styles.header}>
-        <div className={styles.container}>
-    
-            <Link className={styles.logo} href={'homepage'}><img className={styles.logoImg} src="images/logo.png" title="ifootballLogo" /></Link>
-    
-            <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" />
-            <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
-    
-              <ul className={styles.nav}>
-                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-1</Link></li>
-                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-2</Link></li>
-                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-3</Link></li>
-                <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-4</Link></li>
-    
-              </ul>
-        </div>
-        
+      <div className={styles.container}>
+
+        <Link className={styles.logo} href={'homepage'}><img className={styles.logoImg} src="images/logo.png" title="ifootballLogo" /></Link>
+
+        <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" onClick={(() => {
+          setOpenSideBar(!openSideBar);
+        })} />
+        <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
+
+
+        <div></div>
+        {
+          !openSideBar?
+          <li></li>
+            
+            : 
+            <ul className={styles.nav}>
+              <li className={styles.titleOps}><h3>Título</h3></li>
+              <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-1</Link></li>
+              <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-2</Link></li>
+              <li className={styles.titleOps}><h3>Pontuação</h3></li>
+              <li><Link className={styles.ops} href={'homepage'}>Time</Link></li>
+              <li><Link className={styles.ops} href={'homepage'}>Jogador</Link></li>
+              <li><Link className={styles.ops} href={'homepage'}>Gols</Link></li>
+              <li><Link className={styles.ops} href={'homepage'}>Assistência</Link></li>
+              <li><Link className={styles.ops} href={'homepage'}>Defesas</Link></li>
+
+            </ul>
+
+        }
+      </div>
+
+
     </header>
-    
+
   )
 }
