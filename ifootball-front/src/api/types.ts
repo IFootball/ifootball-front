57bc35e8c8ec7e3e26c3303c21dc41114555739a
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-07-08 19:19:43 -0300

    upload

---
+++ ifootball-front/src/api/types.ts
@@ -0,0 +1,9 @@
+export type user_type = {
+    idClass: number,
+    name: string,
+    email: string
+}
+export type error_type = {
+    message: string,
+    statusCode: number
+}
--- ifootball-front/src/app/register/page.tsx
+++ ifootball-front/src/app/register/page.tsx
@@ -4,59 +4,87 @@ import styles from '../../../styles/register.module.scss';
 import Header from '@/components/Header';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import api from '@/api';
 
 export default function Register() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const register = async (event: FormEvent<HTMLFormElement>): Promise<boolean> => {
+  const register = async (event: FormEvent<HTMLFormElement>): Promise<boolean> => {
 
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        console.log("Nome: " + formData.get('user-input') + " | Email: " + formData.get('email-input') + " | Senha: " + formData.get('password-input'))
-        router.push('/register')
-        return true;
-    }
-    return (
+    let data: { [key: string]: string } = {};
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    const response = await api.authentication.createAccount(data);
+    console.log(response)
+    // router.push('/register')
+    return false;
+  }
+  return (
     <div className={styles.Register}>
-        <Header />
+      {/* <Header /> */}
 
-        
 
-        <main className={styles.main}>
+
+      <main className={styles.main}>
 
         <div>
-        <Link className={styles.backLoginLink} href={'../'}>Entrar</Link>
-          </div>
-          
-          
-      <div className={styles.registerUtilArea}>
+          <Link className={styles.backLoginLink} href={'../'}>Entrar</Link>
+        </div>
 
-        <h1 className={styles.registerTitle}>IFootball</h1>
 
-        <form onSubmit={register}  className={styles.registerForm}>
+        <div className={styles.registerUtilArea}>
 
-          <div className={styles.registerField}>
-            <label htmlFor="user-input" className={styles.registerLabel}>Usuário</label>
-            <input className={styles.registerInput} type="text" name="user-input" id="user-input" />
-          </div>
+          <h1 className={styles.registerTitle}>IFootball</h1>
 
-          <div className={styles.registerField}>
-            <label htmlFor="email-input" className={styles.registerLabel}>Email</label>
-            <input className={styles.registerInput} type="email" name="email-input" id="email-input" />
-          </div>
+          <form onSubmit={register} className={styles.registerForm}>
 
-          <div className={styles.registerField}>
-            <label htmlFor="password-input" className={styles.registerLabel}>Senha</label>
-            <input className={styles.registerInput} type="password" name="password-input" id="password-input" />
-          </div>
+            <div className={styles.registerField}>
+              <label htmlFor="name" className={styles.registerLabel}>Usuário</label>
+              <input className={styles.registerInput} minLength={6} maxLength={30} type="text" name="name" id="name" />
+            </div>
 
-          <button type='submit' className={styles.registerButton}>Cadasrar</button>
+            <div className={styles.registerField}>
+              <label htmlFor="email" className={styles.registerLabel}>Email</label>
+              <input className={styles.registerInput} type="email" name="email" id="email" />
+            </div>
 
-        </form>
-      </div>
-    </main>
+            <div className={styles.registerField}>
+              <label htmlFor="password" className={styles.registerLabel}>Senha</label>
+              <input className={styles.registerInput} type="password" name="password" id="password" />
+            </div>
 
-        
+            <div className={styles.registerField}>
+              <label htmlFor="idClass" className={styles.registerLabel}>Turma</label>
+              <select name="idClass" id="idClass">
+                <option value={1}>Técnico em Informática I</option>
+                <option value={2}>Técnico em Informática II</option>
+                <option value={3}>Técnico em Informática III</option>
+                <option value={4}>Técnico em Informática IV</option>
+                <option value={5}>Técnico em Química I</option>
+                <option value={6}>Técnico em Química II</option>
+                <option value={7}>Técnico em Química III</option>
+                <option value={8}>Técnico em Química IV</option>
+                <option value={9}>Técnico em Meio Ambiente I</option>
+                <option value={10}>Técnico em Meio Ambiente II</option>
+                <option value={11}>Técnico em Meio Ambiente III</option>
+                <option value={12}>Técnico em Meio Ambiente IV</option>
+                <option value={13}>Técnico em Administração I</option>
+                <option value={14}>Técnico em Administração II</option>
+                <option value={15}>Técnico em Administração III</option>
+                <option value={16}>Técnico em Administração IV</option>
+              </select>
+            </div>
+
+            <button type='submit' className={styles.registerButton}>Cadastrar</button>
+
+          </form>
+        </div>
+      </main>
+
+
     </div>
-    )
+  )
 }
