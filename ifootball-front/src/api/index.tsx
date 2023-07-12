caed94d2b35dc3f525381dabf505a9c37fdc2689
phgiacomelli <paulohenriquegiacomelli@gmail.com>
2023-07-12 14:33:10 -0300

    subindo ajustes next config e endereços para conectar o back

--- ifootball-front/src/api/index.tsx
+++ ifootball-front/src/api/index.tsx
@@ -2,100 +2,133 @@ import axios from "axios";
 import { error_type, user_type } from "./types";
 
 const simpleBasicFetch = async (endpoint: string, params: string) => {
+  let str = `/${endpoint}`;
 
-    let str = `/${endpoint}`;
-
-    if (params !== '') {
-        str = `${str}?${params}`;
-    }
-    return await api.get(encodeURI(str));
-}
+  if (params !== "") {
+    str = `${str}?${params}`;
+  }
+  return await api.get(encodeURI(str));
+};
 
 type resultType<T> = {
-    conex: boolean,
-    reqStat: number,
-    success: boolean,
-    data: T,
-}
+  conex: boolean;
+  reqStat: number;
+  success: boolean;
+  data: T;
+};
 
-const basicFetch = async (method: string, endpoint: string, params: object, alertConex?: boolean, alertError?: boolean): Promise<resultType<any>> => {
-    let result = {
-        conex: true,//só fica em false quando cai no catch ou não responde nada
-        reqStat: 0,
-        success: false,//Setado em true quando tudo dá certo e status de retorno é 10
-        data: false,//Conteúdo JSON recebido do servidor
-        msg: ''//Inserido no Front -> Codigo de status que diz o que será carregado pra lá.
-    }
+const basicFetch = async (
+  method: string,
+  endpoint: string,
+  params: object,
+  alertConex?: boolean,
+  alertError?: boolean
+): Promise<resultType<any>> => {
+  let result = {
+    conex: true, //só fica em false quando cai no catch ou não responde nada
+    reqStat: 0,
+    success: false, //Setado em true quando tudo dá certo e status de retorno é 10
+    data: false, //Conteúdo JSON recebido do servidor
+    msg: "", //Inserido no Front -> Codigo de status que diz o que será carregado pra lá.
+  };
+  try {
+    let r: any = false;
     try {
-        let r: any = false;
-        try {
-            r = method === 'POST' ? await api.post(`/${endpoint}/`, buildParamString(params), { validateStatus: function (status) { return true; } }) : (method === 'GET' ? await simpleBasicFetch(endpoint, buildParamString(params)) : false);
-        } catch (e: any) {
-            r = e.response;
-        }
-        console.log('API_R: ', r);
-        if (r) {
-            result.reqStat = r.status;
-            result.success = r.status === 200 || r.status === 201;
-            if (r.data) result.data = r.data;
-        } else {
-            result.conex = false;
-        }
-    } catch (e) {
-        console.log('AXIOS ERR: ', e);
-        result.conex = false;
+      r =
+        method === "POST"
+          ? await api.post(`/${endpoint}/`, buildParamString(params), {
+              validateStatus: function (status) {
+                return true;
+              },
+            })
+          : method === "GET"
+          ? await simpleBasicFetch(endpoint, buildParamString(params))
+          : false;
+    } catch (e: any) {
+      r = e.response;
     }
-    if (alertError && result.conex && !result.success) alert(result.msg);
+    console.log("API_R: ", r);
+    if (r) {
+      result.reqStat = r.status;
+      result.success = r.status === 200 || r.status === 201;
+      if (r.data) result.data = r.data;
+    } else {
+      result.conex = false;
+    }
+  } catch (e) {
+    console.log("AXIOS ERR: ", e);
+    result.conex = false;
+  }
+  if (alertError && result.conex && !result.success) alert(result.msg);
 
-    // console.log('API_RESPONSE: ', result);
-    return result;
-}
+  // console.log('API_RESPONSE: ', result);
+  return result;
+};
 
 /**
  * Montar string de parâmetros para URL.
  * @param params JSON com parâmetros para enviar.
  * @returns Parametros no formato para URL.
  */
-const buildParamString = (params: any) => {//##VER## UM MONTE DE CAGADAS DO WILL PRA ARRUMAR AQUI
+const buildParamString = (params: any) => {
+  //##VER## UM MONTE DE CAGADAS DO WILL PRA ARRUMAR AQUI
 
-    let str = '';
-    Object.keys(params).map((key) => {
-        let value = params[key];
-        if (Array.isArray(value)) {
-            value = JSON.stringify(value);
-        } else if (value && typeof value == 'object') {
-            // value = value.toString();
-            if (Object.keys(value).length > 0) {
-                let micstr: string = '['
-                Object.keys(value).map(item => {
-                    micstr = `${micstr}"${item}"="${value[item]}",`;
-                });
-                micstr = micstr.slice(0, -1);//remove o último ',';
-                value = micstr + ']';
-            }
-        }
+  let str = "";
+  Object.keys(params).map((key) => {
+    let value = params[key];
+    if (Array.isArray(value)) {
+      value = JSON.stringify(value);
+    } else if (value && typeof value == "object") {
+      // value = value.toString();
+      if (Object.keys(value).length > 0) {
+        let micstr: string = "[";
+        Object.keys(value).map((item) => {
+          micstr = `${micstr}"${item}"="${value[item]}",`;
+        });
+        micstr = micstr.slice(0, -1); //remove o último ',';
+        value = micstr + "]";
+      }
+    }
 
-        str = `${str}${key}=${value}&`;
-    });
-    str = str.slice(0, -1);//remove o último '&'
-    return str;
-}
+    str = `${str}${key}=${value}&`;
+  });
+  str = str.slice(0, -1); //remove o último '&'
+  return str;
+};
+
+let axiosConfig = {
+  "Content-Type": "application/json;charset=UTF-8",
+  "Access-Control-Allow-Origin": "*",
+};
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/'
-})
+  baseURL: "https://localhost:8888/api",
+  headers: axiosConfig,
+});
 
 export default {
-    authentication: {
-        createAccount: async (data: any): Promise<user_type> => {
-            // return await basicFetch('POST', 'users/', data)
-            const response = await api.post('users/', {
-                idClass: Number(data.idClass),
-                name: data.name,
-                email: data.email,
-                password: data.password
-              });
-            return response.data
-        }
-    }
-}
+  authentication: {
+    createAccount: async (data: any): Promise<any> => {
+      // return await basicFetch('POST', 'users/', data)
+
+      let createUserRequest = {
+        IdClass: Number(data.idClass),
+        Name: data.name,
+        Email: data.email,
+        Password: data.password,
+      };
+
+      await api
+        .post("/users", createUserRequest)
+        .then(function (response) {
+          console.log("res1-------", response);
+
+          return response.data;
+        })
+        .then(function (data) {
+          console.log("res2-------", data);
+          // console.log(data);
+        });
+    },
+  },
+};
--- ifootball-front/src/app/register/page.tsx
+++ ifootball-front/src/app/register/page.tsx
@@ -1,63 +1,68 @@
-'use client';
-import { FormEvent } from 'react';
-import styles from '../../../styles/register.module.scss';
-import Header from '@/components/Header';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import api from '@/api';
+"use client";
+import { FormEvent } from "react";
+import styles from "../../../styles/register.module.scss";
+import Header from "@/components/Header";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import api from "@/api";
 
 export default function Register() {
   const router = useRouter();
 
-  const register = async (event: FormEvent<HTMLFormElement>): Promise<boolean> => {
-
+  const register = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<boolean> => {
     let data: { [key: string]: string } = {};
+
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+
     formData.forEach((value, key) => {
       data[key] = value;
     });
+
     const response = await api.authentication.createAccount(data);
-    console.log(response)
+
+    console.log(response);
     // router.push('/register')
     return false;
-  }
+  };
   return (
-    <div className={styles.Register}>
+    <div>
       {/* <Header /> */}
 
-
-
-      <main className={styles.main}>
-
+      <main>
         <div>
-          <Link className={styles.backLoginLink} href={'../'}>Entrar</Link>
+          <Link href={"../"}>Entrar</Link>
         </div>
 
+        <div>
+          <h1>IFootball</h1>
 
-        <div className={styles.registerUtilArea}>
-
-          <h1 className={styles.registerTitle}>IFootball</h1>
-
-          <form onSubmit={register} className={styles.registerForm}>
-
-            <div className={styles.registerField}>
-              <label htmlFor="name" className={styles.registerLabel}>Usuário</label>
-              <input className={styles.registerInput} minLength={6} maxLength={30} type="text" name="name" id="name" />
+          <form onSubmit={register}>
+            <div>
+              <label htmlFor="name">Usuário</label>
+              <input
+                minLength={6}
+                maxLength={30}
+                type="text"
+                name="name"
+                id="name"
+              />
             </div>
 
-            <div className={styles.registerField}>
-              <label htmlFor="email" className={styles.registerLabel}>Email</label>
-              <input className={styles.registerInput} type="email" name="email" id="email" />
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" />
             </div>
 
-            <div className={styles.registerField}>
-              <label htmlFor="password" className={styles.registerLabel}>Senha</label>
-              <input className={styles.registerInput} type="password" name="password" id="password" />
+            <div>
+              <label htmlFor="password">Senha</label>
+              <input type="password" name="password" id="password" />
             </div>
 
-            <div className={styles.registerField}>
-              <label htmlFor="idClass" className={styles.registerLabel}>Turma</label>
+            <div>
+              <label htmlFor="idClass">Turma</label>
               <select name="idClass" id="idClass">
                 <option value={1}>Técnico em Informática I</option>
                 <option value={2}>Técnico em Informática II</option>
@@ -78,13 +83,10 @@ export default function Register() {
               </select>
             </div>
 
-            <button type='submit' className={styles.registerButton}>Cadastrar</button>
-
+            <button type="submit">Cadastrar</button>
           </form>
         </div>
       </main>
-
-
     </div>
-  )
+  );
 }
