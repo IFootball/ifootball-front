05f7e8df2b4a48c5c31f6de3114e16f17ef4d61c
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2023-07-12 09:11:42 -0300

    headerAgain

--- ifootball-front/src/components/Header/index.tsx
+++ ifootball-front/src/components/Header/index.tsx
@@ -15,35 +15,44 @@ export default function Navbar() {
       <div className={styles.container}>
 
         <div className={styles.background}>
-        <Link className={styles.logo} href={'homepage'}><img className={styles.logoImg} src="images/logo.png" title="ifootballLogo" /></Link>
+          <div className={styles.imgDiv}>
+            <Link className={styles.logo} href={'homepage'}><img className={styles.logoImg} src="images/logoFoot.png" title="ifootballLogo" /></Link>
+          </div>
 
-        <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" onClick={(() => {
-          setOpenSideBar(!openSideBar);
-        })} />
-        <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
+          <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" onClick={(() => {
+            setOpenSideBar(!openSideBar);
+          })} />
+          <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
         </div>
 
         {
-          !openSideBar?
-          <div className={styles.none}></div>
-            
-            :
-            
-            <div className={styles.backgroundMenu}>
-            <ul className={styles.nav}>
-              <li className={styles.titleOps}><h3>Título</h3></li>
-              <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-1</Link></li>
-              <li><Link className={styles.ops} href={'homepage'}>OPÇÃO-2</Link></li>
-              <li className={styles.titleOps}><h3>Pontuação</h3></li>
-              <li><Link className={styles.ops} href={'homepage'}>Time</Link></li>
-              <li><Link className={styles.ops} href={'homepage'}>Jogador</Link></li>
-              <li><Link className={styles.ops} href={'homepage'}>Gols</Link></li>
-              <li><Link className={styles.ops} href={'homepage'}>Assistência</Link></li>
-              <li><Link className={styles.ops} href={'homepage'}>Defesas</Link></li>
+          !openSideBar ?
+            <div className={styles.none}></div>
 
-            </ul>
+            :
+
+            <div className={styles.backgroundMenu}>
+              <ul className={styles.nav}>
+                <li className={styles.titleOps}><h3>TÍTULO</h3></li>
+                <li><Link className={styles.ops} href={'homepage'}>Opção1</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>Opção2</Link></li>
+                <li className={styles.titleOps}><h3>PONTUAÇÃO</h3></li>
+                <li><Link className={styles.ops} href={'homepage'}>Time</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>Jogador</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>Gols</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>Assistência</Link></li>
+                <li><Link className={styles.ops} href={'homepage'}>Defesas</Link></li>
+
+              </ul>
+
+              <div className={styles.outDiv}>
+                <a className={styles.outA} href=""><div className={styles.outDivA}><img className={styles.outImg} src="images/out.png" title="outLogo" /><h4 className={styles.outH4}>SAIR</h4></div></a> 
+              </div>
+
+              
+
             </div>
-            
+
 
         }
       </div>
