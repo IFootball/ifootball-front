cefe8bf26f5ec5bf84bc76bc3f065ac3157743b9
MuriloMoutinho <104277785+MuriloMoutinho@users.noreply.github.com>
2023-07-18 18:38:37 -0300

    menu lateral

--- ifootball-front/src/components/Header/index.tsx
+++ ifootball-front/src/components/Header/index.tsx
@@ -11,23 +11,18 @@ export default function Navbar() {
   return (
 
     <header className={styles.header}>
-      <div>
         <div className={styles.background}>
-          <div className={styles.imgDiv}>
-            <Link href={'homepage'}><img className={styles.logoImg} src="images/logoFoot.png" title="ifootballLogo" /></Link>
-          </div>
-
           <input className={styles.mobile_btn} type="checkbox" id="mobile_btn" onClick={(() => {
             setOpenSideBar(!openSideBar);
           })} />
           <label className={styles.mobile_icon} htmlFor="mobile_btn" ><span className={styles.hamburguer}></span></label>
+
+            <Link href={'homepage'}><img src="images/logoFoot.png" title="ifootballLogo" /></Link>
+
         </div>
 
-        {
-          !openSideBar ?
-            <div></div>
-            :
-            <div className={styles.backgroundMenu}>
+        
+            <div className={`${styles.backgroundMenu} ${!openSideBar && styles.backgroundMenuClose}`}>
               <ul className={styles.nav}>
                 <li className={styles.titleOps}>
                   <div className={styles.menu_in_menu}>
@@ -38,15 +33,16 @@ export default function Navbar() {
 
                 <li className={styles.titleOps}>
                   <div className={styles.menu_in_menu}>
-                    <input className={styles.mobile_btn_ops} type="checkbox" id="mobile_btn_ops" onClick={(() => {
-                      setOpenMenuBar(!openMenuBar);
-                    })}></input><label className={styles.mobile_icon_ops} htmlFor="mobile_btn_ops" >PONTUAÇÃO<p className={styles.hamburguer_ops}>▼</p></label>
+                    <input className={styles.mobile_btn_ops} type="checkbox" id="mobile_btn_ops" 
+                    onClick={() => setOpenMenuBar(!openMenuBar)}></input>
+                    <label className={styles.mobile_icon_ops} htmlFor="mobile_btn_ops" >
+                      <p className={`${styles.hamburguer_ops} ${openMenuBar && styles.menu_bar_open}`}>▶</p>
+                      PONTUAÇÃO
+                    </label>
 
                     <div>
                       {
-                        !openMenuBar ?
-                          <div></div>
-                          :
+                        openMenuBar &&
                           <div className={styles.menu_in_menu_ops}>
                             <Link className={styles.ops} href={'homepage'}>Time</Link>
                             <Link className={styles.ops} href={'homepage'}>Jogador</Link>
@@ -64,8 +60,7 @@ export default function Navbar() {
                 <Link className={styles.outA} href="../"><img className={styles.outImg} src="images/out.png" title="outLogo" /><h4 className={styles.outH4}>SAIR</h4></Link>
               </div>
             </div>
-        }
-      </div>
+        
     </header>
   )
 }
