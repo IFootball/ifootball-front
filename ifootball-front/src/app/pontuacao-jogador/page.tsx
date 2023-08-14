5e96dde3e0e898d3644157675f7437f70b078e7e
marceloost <104876213+marceloost@users.noreply.github.com>
2023-08-14 17:58:24 -0300

    up

--- ifootball-front/src/app/pontuacao-jogador/page.tsx
+++ ifootball-front/src/app/pontuacao-jogador/page.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import styles from "../../../styles/pontuacaoJogador.module.scss";
-import GlobalCard from "@/components/globalCard";
-import Link from "next/link";
-import DefaultButton from "@/components/DefaultButton";
 import pontuacaoStyles from "./pontuacaoJogador.module.scss";
 
 export default function PontuacaoJogador() {
@@ -10,57 +7,60 @@ export default function PontuacaoJogador() {
     const numCols = 4;
 
     return (
-        <div className={styles.pontuacaoJog}>
-            <div className={styles.pontuacaoJogUtil}>
+        <div className={styles.container}>
+            <div className={styles.pontuacaoJog}>
+                <div className={styles.pontuacaoJogUtil}>
+                    <div className={styles.posicao}>
+                        <h3>LINHA</h3>
+                    </div>
+                    <div className={styles.nomeJog}>
+                        <h1>NOME</h1>
+                    </div>
+                        <div className={styles.fotoJog}>
+                    </div>
+                </div>
 
-                <div className={styles.posicao}>
-                    <h3>LINHA</h3>
+                <div className={styles.tabelaPontuacao}>
+                <div className={styles.pontuacaoTotal}>
+                    <h5>TOTAL:</h5>
                 </div>
-                <div className={styles.nomeJog}>
-                    {/*<pontuacaoJogador nomeJogador={nomeJogador} />*/} <h1>NOME</h1>
+                    <table>
+                        <thead>
+                            <tr className={styles.gol}>
+                                <th>GOL</th>
+                                <th>00.00</th>
+                                <th><button className={styles.somar}>+</button></th>
+                                <th><button className={styles.subtrair}>-</button></th>
+                            </tr>
+                            <tr className={styles.assistencia}>
+                                <th>ASSISTÊNCIA</th>
+                                <th>00.00</th>
+                                <th><button className={styles.somar}>+</button></th>
+                                <th><button className={styles.subtrair}>-</button></th>
+                            </tr>
+                            <tr className={styles.falta}>
+                                <th>FALTA</th>
+                                <th>00.00</th>
+                                <th><button className={styles.subtrair}>-</button></th>
+                                <th><button className={styles.somar}>+</button></th>
+                            </tr>
+                            <tr className={styles.amarelo}>
+                                <th>AMARELO</th>
+                                <th>00.00</th>
+                                <th><button className={styles.subtrair}>-</button></th>
+                                <th><button className={styles.somar}>+</button></th>
+                            </tr>
+                            <tr className={styles.vermelho}>
+                                <th>VERMELHO</th>
+                                <th>00.00</th>
+                                <th><button className={styles.subtrair}>-</button></th>
+                                <th><button className={styles.somar}>+</button></th>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
-                <div className={styles.fotoJog}>
-                    {/*<Link href={'homepage'}><img src="images/logoFoot.png" title="ifootballLogo" /></Link>*/}
-                </div>
-                <table></table>
-            </div>
 
-            <div className={styles.tabelaPontuacao}>
-                <table>
-                    <thead>
-                        <tr className={styles.gol}>
-                            <th>GOL</th>
-                            <th>00.00</th>
-                            <th><button className={styles.somar}>+</button></th>
-                            <th><button className={styles.subtrair}>-</button></th>
-                        </tr>
-                        <tr className={styles.assistencia}>
-                            <th>ASSISTÊNCIA</th>
-                            <th>00.00</th>
-                            <th><button className={styles.somar}>+</button></th>
-                            <th><button className={styles.subtrair}>-</button></th>
-                        </tr>
-                        <tr className={styles.falta}>
-                            <th>FALTA</th>
-                            <th>00.00</th>
-                            <th><button className={styles.subtrair}>-</button></th>
-                            <th><button className={styles.somar}>+</button></th>
-                        </tr>
-                        <tr className={styles.amarelo}>
-                            <th>AMARELO</th>
-                            <th>00.00</th>
-                            <th><button className={styles.subtrair}>-</button></th>
-                            <th><button className={styles.somar}>+</button></th>
-                        </tr>
-                        <tr className={styles.vermelho}>
-                            <th>VERMELHO</th>
-                            <th>00.00</th>
-                            <th><button className={styles.subtrair}>-</button></th>
-                            <th><button className={styles.somar}>+</button></th>
-                        </tr>
-                    </thead>
-                    <div className={styles.confirmar}>CONFIRMAR</div>
-                </table>
+                <div className={styles.confirmar}>CONFIRMAR</div>
             </div>
         </div>
     );
