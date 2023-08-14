2edd698529a945a7bde8e14440117bf82d118566
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-08-14 20:16:11 -0300

    list players

--- ifootball-front/src/app/squad/components/PlayerComponent/index.tsx
+++ ifootball-front/src/app/squad/components/PlayerComponent/index.tsx
@@ -12,7 +12,7 @@ interface playerProps {
     squad: number[],
     gkId: number
 }
-export default function PlayerComponent({player, isCaptain = false, addPlayer, dispensePlayer, setAsCaptain, unsetAsCaptain, squad, gkId}: playerProps) {
+export default function PlayerComponent({ player, isCaptain = false, addPlayer, dispensePlayer, setAsCaptain, unsetAsCaptain, squad, gkId }: playerProps) {
     return (
         <div className={style.playerCard}>
             {/* <Image src={goalkeeper.image} alt={`${goalkeeper.name}-image`} quality={100} width={85} height={70} placeholder='blur' /> */}
@@ -25,25 +25,20 @@ export default function PlayerComponent({player, isCaptain = false, addPlayer, d
             </div>
             <div className={style.playerConfigs}>
                 {
-                    player.playerType === CONSTS.playerTypes.lineplayer &&
-                    <>
-                        {
-                            isCaptain  ?
-                            <div className={`${style.captainButton} ${style.active}`} onClick={() => unsetAsCaptain(player.id)}>
-                                C
-                            </div>
-                            :
-                            <div className={style.captainButton} onClick={() => setAsCaptain(player.id)}>
-                                C
-                            </div>
-                        }
-                    </>
+                    isCaptain ?
+                        <div className={`${style.captainButton} ${style.active}`} onClick={() => unsetAsCaptain(player.id)}>
+                            C
+                        </div>
+                        :
+                        <div className={style.captainButton} onClick={() => setAsCaptain(player.id)}>
+                            C
+                        </div>
                 }
                 {
                     ((player.playerType === CONSTS.playerTypes.lineplayer) && squad.includes(player.id)) || ((player.playerType === CONSTS.playerTypes.goalkeeper) && (gkId === player.id)) ?
-                    <DefaultButton small dispensed text='DISPENSAR' action={dispensePlayer} />
-                    :
-                    ((player.playerType === CONSTS.playerTypes.lineplayer && squad.length <= 4) || ((player.playerType === CONSTS.playerTypes.goalkeeper) && gkId === 0) ? <DefaultButton small text='ESCALAR' action={addPlayer} /> : <DefaultButton small text='ESCALAR' unactive />) 
+                        <DefaultButton small dispensed text='DISPENSAR' action={dispensePlayer} />
+                        :
+                        ((player.playerType === CONSTS.playerTypes.lineplayer && squad.length <= 4) || ((player.playerType === CONSTS.playerTypes.goalkeeper) && gkId === 0) ? <DefaultButton small text='ESCALAR' action={addPlayer} /> : <DefaultButton small text='ESCALAR' unactive />)
                 }
             </div>
         </div>
--- ifootball-front/src/app/squad/male/page.tsx
+++ ifootball-front/src/app/squad/male/page.tsx
@@ -45,6 +45,7 @@ export default function Male() {
         listGoalkeepers();
         listLinePlayers();
         verifySession();
+        console.log("JOGADORES: " + JSON.stringify(players), "GOLEIROS: " + JSON.stringify(goalkeepers))
     }, [])
     return (
         <div className={styles.maleSquadPage}>
