82560e6d096912b13ae821968d53ed7f10db1952
MuriloMoutinho <104277785+MuriloMoutinho@users.noreply.github.com>
2023-08-19 11:28:01 -0300

    integração com api funcionando

--- ifootball-front/src/api/types.ts
+++ ifootball-front/src/api/types.ts
@@ -1,36 +1,60 @@
 export type user_type = {
-    idClass: number,
-    name: string,
-    email: string
-}
+  idClass: number;
+  name: string;
+  email: string;
+};
 export type error_type = {
-    message: string,
-    statusCode: number
-}
+  message: string;
+  statusCode: number;
+};
 
 export type classes_type = {
-    id: number,
-    name: string
-}
+  id: number;
+  name: string;
+};
 
 export type JWTToken = {
-    Id: string,
-    exp: number,
-    iat: number,
-    nbf: number,
-    role: string
-}
+  Id: string;
+  exp: number;
+  iat: number;
+  nbf: number;
+  role: string;
+};
 
 // 0 - Jogador normal | 1 - goleiro
 // 1 - machos | 2 - fêmeas
 
 export type playerType = {
-    id: number,
-    name: string,
-    image: string,
-    className: string,
-    idTeamClass: number,
-    idGender: number,
-    playerType: number,
-    inSquad?: boolean
+  id: number;
+  name: string;
+  image: string;
+  className: string;
+  idTeamClass: number;
+  idGender: number;
+  playerType: number;
+  inSquad?: boolean;
+};
+
+export type completePlayerScout = {
+  id: number;
+  name: string;
+  image: string;
+  className: string;
+  idTeamClass: number;
+  idGender: number;
+  playerType: number;
+  assists: number;
+  fouls: number;
+  goals: number;
+  redCard: number;
+  wins: number;
+  yellowCard: number;
+  takenGols: number;
+  penaltySaves: number;
+  saves: number;
+};
+
+export enum playerTypeEnum {
+  Linha = 0,
+  Goleiro = 1,
 }
