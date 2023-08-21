54a97097bb80b2f2eda2a156c5e0ad72438d25a3
marceloost <104876213+marceloost@users.noreply.github.com>
2023-08-21 15:45:47 -0300

    up

---
+++ ifootball-front/src/app/ranking-times/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from "../../../styles/rankingTimes.scss";
+import Header from "@/components/Header";
+
+const mockRankingData = [
+  { posição: 1, jogador: 'Jogador A', pontuação: 100 },
+  { posição: 2, jogador: 'Jogador B', pontuação: 90 },
+  { posição: 3, jogador: 'Jogador C', pontuação: 80 }
+];
+
+export default function Home() {
+  return (
+    <div className={styles.HomePage}>
+      <Header />
+      <div className={styles.homeUtil}>
+        <div className={styles.titulo}>
+          <h3>TOP PONTUAÇÃO POR TIME</h3>
+        </div>
+        <div className={styles.ranking}>
+          <table>
+            <thead>
+              <tr>
+                <th>Posição</th>
+                <th>Jogador</th>
+                <th>Pontuação</th>
+              </tr>
+            </thead>
+            <tbody>
+              {mockRankingData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.posição}</td>
+                  <td>{item.jogador}</td>
+                  <td>{item.pontuação}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
