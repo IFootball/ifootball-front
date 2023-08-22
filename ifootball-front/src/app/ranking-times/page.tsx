f1926ffc0f715e3e95999efd4cd038a548c22fbf
marceloost <104876213+marceloost@users.noreply.github.com>
2023-08-22 15:44:49 -0300

    up

--- ifootball-front/src/app/ranking-times/page.tsx
+++ ifootball-front/src/app/ranking-times/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from "../../../styles/rankingTimes.module.scss";
 import Header from "@/components/Header";
+import Image from "next/image";
+import primeiro from "../imagens/primeiro.png";
+import segundo from "../imagens/segundo.png";
+import terceiro from "../imagens/terceiro.png";
 
 const mockRankingData = [
-  { posição: 1, jogador: 'Jogador A', pontuação: 100 },
-  { posição: 2, jogador: 'Jogador B', pontuação: 90 },
-  { posição: 3, jogador: 'Jogador C', pontuação: 80 }
+  { posição: <Image src={primeiro} alt="primeiro" />, jogador: 'Jogador A', pontuação: 100 },
+  { posição: <Image src={segundo} alt="segundo" />, jogador: 'Jogador B', pontuação: 90 },
+  { posição: <Image src={terceiro} alt="terceiro" />, jogador: 'Jogador C', pontuação: 80 }
 ];
 
 export default function Home() {
@@ -18,24 +22,24 @@ export default function Home() {
             <h3>TOP PONTUAÇÃO POR TIME</h3>
             </div>
             <div className={styles.ranking}>
-            <table>
-                <thead>
-                <tr>
-                    <th>Posição</th>
-                    <th>Jogador</th>
-                    <th>Pontuação</th>
-                </tr>
-                </thead>
-                <tbody>
-                {mockRankingData.map((item, index) => (
-                    <tr key={index}>
-                    <td>{item.posição}</td>
-                    <td>{item.jogador}</td>
-                    <td>{item.pontuação}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+              <table className={styles.tabela}>
+                  <thead>
+                  <tr>
+                      <th>Posição</th>
+                      <th>Jogador</th>
+                      <th>Pontuação</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {mockRankingData.map((item, index) => (
+                      <tr key={index}>
+                      <td>{item.posição}</td>
+                      <td>{item.jogador}</td>
+                      <td>{item.pontuação}</td>
+                      </tr>
+                  ))}
+                  </tbody>
+              </table>
             </div>
         </div>
         <div className={styles.voltar}>VOLTAR</div>
