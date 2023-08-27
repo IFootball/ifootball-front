32360d84cb1961be38eadddea364a78be7cc3f09
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-08-27 14:53:23 -0300

    upload

--- ifootball-front/src/app/page.tsx
+++ ifootball-front/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import style from '../../styles/intro.module.scss';
+import logo from '../../public/images/logoFoot.png'
 const Intro = () => {
     return (
         <div className={style.introPage}>
-
+            <Image src={logo} alt='Logo IFootball' />
         </div>
     )
 }
