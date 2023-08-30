384ef47c47a8810dde4dba35cac0659e38c51bea
MuriloMoutinho <104277785+MuriloMoutinho@users.noreply.github.com>
2023-08-29 22:51:48 -0300

    campo responsivel

--- ifootball-front/src/components/Campo/index.tsx
+++ ifootball-front/src/components/Campo/index.tsx
@@ -158,10 +158,10 @@ export default function Campo({
 
     return (
         <>
-            <div className={style.field}>
-                <ToastContainer
+            <ToastContainer
                 theme="colored"
-                />
+            />
+            <div className={style.field}>
                 <div className={`${style.lateral} ${style.linha}`}>
                     <div className={`${style.linha} ${style.area} ${style.left}`}></div>
                     <div className={`${style.circulo} ${style.linha}`}></div>
